5771f089e974645131cdc71a806518096db14a6f
yuan-co-tokyo <yuan.co.tokyo@gmail.com>
2025-11-22 16:07:54 +0900

    first commit

---
+++ movie-app/src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Header from "./Header.tsx";
+import MovieDetail from "./MovieDetail.tsx";
+import App from './App.tsx';
+
+const router = createBrowserRouter([
+  { path: "/", Component: App },
+  { path: "/movies/:movieId", Component: MovieDetail },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Header>
+      <RouterProvider router={router} />
+    </Header>
+  </StrictMode>,
+)
